c254e15583679adc111357eb5819ca883084cc82
Movindu Senura <107047916+MovinduSenura@users.noreply.github.com>
2025-05-02 13:41:19 +0530

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1746173254,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/background-sheet0.png",
+		"images/button-sheet0.png",
+		"images/pressingarea-sheet0.png",
+		"images/gameoverblock-sheet0.png",
+		"images/start-sheet0.png",
+		"images/startbtn3-sheet0.png",
+		"media/pagalworld.com.mx-crazy frog axel f.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png"
+	]
+}
